a5e467e94d7e0e9d3d1765246a2e2d7b0e422899
Shem Itoya <rachet.clank001@gmail.com>
2023-10-15 06:53:27 +0100

    add separate landing page for routing ease

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as textData from "src/assets/jsondata/textDataDB.json"
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  accordionText:any = textData
+  accordionBody: string = this.accordionText[0].homePage.accordion;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
