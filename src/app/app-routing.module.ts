ca3fc44d7bdfb47e775ed37332147ae577558410
Shem Itoya <rachet.clank001@gmail.com>
2023-10-15 06:54:12 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { WorkComponent } from './pages/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
