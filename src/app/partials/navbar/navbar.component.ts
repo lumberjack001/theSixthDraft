05029c39088b78b9a296c4b915ac556b0e7cd7a1
Shem Itoya <rachet.clank001@gmail.com>
2023-10-16 19:50:06 +0100

    add navbar collapse and responsiveness fixes on blog, project page and footer

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  hide: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toggleNav(){
+    if(this.hide === false){
+      this.hide = true;
+    }else{
+      this.hide = false
+    }
+  }
 }
